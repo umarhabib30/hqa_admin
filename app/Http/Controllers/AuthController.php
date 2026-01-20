16afc480956323cb4f6bec07930cbc46a22c0c46
M Umar Habib <100488355+umarhabib30@users.noreply.github.com>
2026-01-20 17:16:49 +0500

    otp login updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
             ]);
         }
         
-        return redirect()->route('dashboard.index');
+        // return redirect()->route('dashboard.index');
         $user = Auth::user();
         Auth::logout(); // login after OTP only
 
